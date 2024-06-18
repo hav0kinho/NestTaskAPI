26c38d94df6a8106dc63fcc8b1acd25db48d409b
hav0kinho <ruallysonfelype@gmail.com>
2024-06-18 12:25:16 -0300

    adicionando comentário para explicar o ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Faz o NestJS utilizar o ValidationPipe, que irá utilizar as validações oque você configurou com o Class Validator nas DTO's
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(3000);

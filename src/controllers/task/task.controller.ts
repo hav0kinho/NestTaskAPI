440a98f0a3c4a86a0723301e85831e806b4f3a44
hav0kinho <ruallysonfelype@gmail.com>
2024-06-18 13:46:40 -0300

    Criação dos endpoints e integração com o banco de dados

--- src/controllers/task/task.controller.ts
+++ src/controllers/task/task.controller.ts
@@ -1,6 +1,19 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Patch,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { TaskService } from './task.service';
 import { CreateTaskDTO } from './dto/create-task.dto';
+import { UUID } from 'crypto';
+import { UpdateTaskDTO } from './dto/update-task.dto';
+import { UpdatePutTaskDTO } from './dto/update-put-task.dto';
 
 @Controller('tasks')
 export class TaskController {
@@ -8,11 +21,32 @@ export class TaskController {
 
   @Get()
   async getTasks() {
-    return await this.taskService.getAllTasks();
+    return await this.taskService.getAll();
   }
 
   @Post()
   async createTask(@Body() data: CreateTaskDTO) {
-    return this.taskService.createTask(data);
+    return this.taskService.create(data);
+  }
+
+  @Delete('/:id')
+  async deleteTask(@Param('id', ParseUUIDPipe) id: UUID) {
+    return await this.taskService.delete(id);
+  }
+
+  @Put('/:id')
+  async updatePutTask(
+    @Param('id', ParseUUIDPipe) id: UUID,
+    @Body() task: UpdateTaskDTO,
+  ) {
+    return await this.taskService.updatePut(id, task);
+  }
+
+  @Patch('/:id')
+  async updatePatchTask(
+    @Param('id', ParseUUIDPipe) id: UUID,
+    @Body() task: UpdatePutTaskDTO,
+  ) {
+    return await this.taskService.updatePatch(id, task);
   }
 }
